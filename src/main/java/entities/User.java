2e242967c28b5b96e573f7af21afd212c391c6e9
Chriswihu <chris.wihu@gmail.com>
2023-05-01 20:07:30 +0200

    UseCase Diagram

--- src/main/java/entities/User.java
+++ src/main/java/entities/User.java
@@ -3,24 +3,21 @@ package entities;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
-import javax.persistence.Basic;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
-import javax.persistence.ManyToMany;
-import javax.persistence.Table;
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import org.mindrot.jbcrypt.BCrypt;
 
 @Entity
 @Table(name = "users")
+@NamedQuery(name = "User.deleteAllRows", query = "DELETE from User")
 public class User implements Serializable {
 
   private static final long serialVersionUID = 1L;
   @Id
+
+  @Column(name = "user_id", nullable = false)
+  private int id;
   @Basic(optional = false)
   @NotNull
   @Column(name = "user_name", length = 25)
@@ -30,6 +27,7 @@ public class User implements Serializable {
   @Size(min = 1, max = 255)
   @Column(name = "user_pass")
   private String userPass;
+
   @JoinTable(name = "user_roles", joinColumns = {
     @JoinColumn(name = "user_name", referencedColumnName = "user_name")}, inverseJoinColumns = {
     @JoinColumn(name = "role_name", referencedColumnName = "role_name")})
@@ -49,16 +47,29 @@ public class User implements Serializable {
 
   public User() {}
 
-  //TODO Change when password is hashed
-   public boolean verifyPassword(String pw){
-    return BCrypt.checkpw(pw, userPass);
-    }
-
   public User(String userName, String userPass) {
     this.userName = userName;
     this.userPass = BCrypt.hashpw(userPass, BCrypt.gensalt());
   }
 
+  //TODO Change when password is hashed
+   public boolean verifyPassword(String pw){
+    return BCrypt.checkpw(pw, userPass);
+    }
+
+  public User(int id, String userName, String userPass) {
+    this.id = id;
+    this.userName = userName;
+    this.userPass = BCrypt.hashpw(userPass, BCrypt.gensalt());
+  }
+
+  public int getId() {
+    return id;
+  }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 
   public String getUserName() {
     return userName;
@@ -87,5 +98,15 @@ public class User implements Serializable {
   public void addRole(Role userRole) {
     roleList.add(userRole);
   }
+
+  @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", userName=" + userName +
+                ", userPass=" + userPass +
+                ", roleList=" + roleList +
+                '}';
+    }
 
 }
